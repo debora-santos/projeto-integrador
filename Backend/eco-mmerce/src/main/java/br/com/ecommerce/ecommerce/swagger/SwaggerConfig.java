5867d626bcfa7e4d42ccb2dde4931b909c37f8f9
Igor Silveira <Digital@DH-A2-20.dhbrasil.digitalhouse.com>
2019-07-23 14:16:36 -0300

    Swagger e testes dos CRUDs de produto e cliente

---
+++ Backend/eco-mmerce/src/main/java/br/com/ecommerce/ecommerce/swagger/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package br.com.ecommerce.ecommerce.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.generation.brazil.ecommerce"))
+                .paths(PathSelectors.any())
+                .build()/*.apiInfo(info())*/;
+    }
+
+    // TODO - Informaçaõ quanto ao grupo
+    /*
+    private ApiInfo info() {
+        return new ApiInfoBuilder().title("Eco-mmerce")
+                .description("Um projeto para a primeira turma da Generation Brasil.")
+                .contact(new Contact("Jorge Ferreira",
+                        "www.jorgeferreira.com.br",
+                        "contato@jorgeferreira.com.br"))
+                .license("MIT License")
+                .licenseUrl("https://opensource.org/licenses/MIT")
+                .version("0.0.1")
+                .build();
+    }
+    */
+}
